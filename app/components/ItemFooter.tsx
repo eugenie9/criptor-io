36e9ed86bc5fc94e7df04ba486bfbb533522e1d6
Yavuz <yavuuztuncil@gmail.com>
2023-11-26 01:54:27 +0300

    publisher imgs served with next

--- app/components/ItemFooter.tsx
+++ app/components/ItemFooter.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import { getHowManyTimePassed, getSource } from "../utils";
+import Image from "next/image";
 
 export default function ItemFooter({ item }: { item: TArticle }) {
   return (
     <div className="flex items-end justify-between mt-4 flex-1 ">
       <Link href={`/publisher/${item.source}`} prefetch={false}>
         <div className="flex items-center">
-          <img
+          <Image
             src={getSource(item.source).logo}
             alt={item.title}
-            className="w-8 h-8 mr-2"
+            width={32}
+            height={32}
+            className="mr-2"
           />
           <span className="font-semibold">{getSource(item.source).name}</span>
         </div>
--- app/utils.ts
+++ app/utils.ts
@@ -4,55 +4,55 @@ const getSource = (id: string) => {
       return {
         name: "BeInCrypto",
         img: "https://beincrypto.com/feed/",
-        logo: "https://pbs.twimg.com/profile_images/1625052512217604096/40L-tsAx_400x400.jpg",
+        logo: "/images/beincrypto-logo.jpeg",
       };
     case "bitcoin_news":
       return {
         name: "Bitcoin News",
         img: "https://news.bitcoin.com/feed/",
-        logo: "https://cryptomeister.com/wp-content/uploads/bitcoin-com-logo-1.png",
+        logo: "/images/bitcoin-news-logo.png",
       };
     case "bitcoin_magazine":
       return {
         name: "Bitcoin Magazine",
         img: "https://bitcoinmagazine.com/feed",
-        logo: "https://play-lh.googleusercontent.com/70cmIQiHpCI5TSDTrDYWYKhJhlLMtsh1A-XAu9JIgFh_EXbForpcbBxXiT9XzVCaRW4",
+        logo: "/images/bitcoin-magazine-logo.png",
       };
     case "coin_gape":
       return {
         name: "Coin Gape",
         img: "https://coingape.com/feed/",
-        logo: "https://pbs.twimg.com/profile_images/1515280697468112898/fbw5bH_J_400x400.jpg",
+        logo: "/images/coin-gape-logo.jpeg",
       };
     case "crypto_potato":
       return {
         name: "Crypto Potato",
         img: "https://cryptopotato.com/feed/",
-        logo: "https://images.crunchbase.com/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/roa33uraq1nbt0c35gby",
+        logo: "/images/crypto-potato-logo.webp",
       };
     case "crypto_slate":
       return {
         name: "Crypto Slate",
         img: "https://cryptoslate.com/feed/",
-        logo: "https://cryptoslate.com/wp-content/uploads/2018/05/c-logo.jpg",
+        logo: "/images/crypto-slate-logo.jpeg",
       };
     case "defiant":
       return {
         name: "The Defiant",
         img: "https://thedefiant.substack.com/feed/",
-        logo: "https://images.contactout.com/companies/b51893061168f2899b10e66322ffdbb9",
+        logo: "/images/defiant-logo.jpeg",
       };
     case "forkast":
       return {
         name: "Forkast",
         img: "https://forkast.news/feed/",
-        logo: "https://forkast.news/wp-content/uploads/2021/06/Forkast-Logo-Only.png",
+        logo: "/images/forkast-logo.png",
       };
     case "protos":
       return {
         name: "Protos",
         img: "https://protos.com/feed/",
-        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbYL3ZAxSMaBxFLMMOlHVrJ5c6tSzMMFuhIjsp1y5JEALgDMY_CWr0onnUSiqhgprvqiY",
+        logo: "/images/protos-logo.png",
       };
     default:
       return {
