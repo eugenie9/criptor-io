775c60cfa46f6f64dffed36cda8be00477dce4fe
Yavuz <yavuuztuncil@gmail.com>
2023-11-26 01:18:29 +0300

    types declaration

---
+++ types.d.ts
@@ -0,0 +1,19 @@
+type TArticle = {
+  id: string;
+  title: string;
+  content: string;
+  url: string;
+  thumbnail: string;
+  date: number;
+  source: string;
+};
+
+type TLastEvaluatedKeyForAllSources = {
+  pubDate: string;
+  date: number;
+};
+
+type TLastEvaluatedKeyForASource = {
+  source: string;
+  date: number;
+};
