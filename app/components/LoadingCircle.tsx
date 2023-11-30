e7c6b2dc7719817dd0ad4c822f14508ed34a3a8c
Yavuz <yavuuztuncil@gmail.com>
2023-11-30 13:42:51 +0300

    loading circle component

---
+++ app/components/LoadingCircle.tsx
@@ -0,0 +1,7 @@
+export default function LoadingCircle() {
+  return (
+    <div className="flex justify-center pb-4">
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+    </div>
+  );
+}
