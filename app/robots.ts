c9378fbe1dbcdcb123f7fa2460d2f5714bee0a0b
Yavuz <yavuuztuncil@gmail.com>
2023-12-25 18:07:38 +0300

    default url changed to www

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/", // Allow everything
     },
-    sitemap: "https://criptor.io/sitemap.xml",
+    sitemap: "https://wwww.criptor.io/sitemap.xml",
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ const publishers: {
   changeFrequency: "always";
 }[] = sources.map((source) => {
   return {
-    url: `https://criptor.io/publisher/${source.id}`,
+    url: `https://www.criptor.io/publisher/${source.id}`,
     changeFrequency: "always",
   };
 });
@@ -14,27 +14,27 @@ const publishers: {
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://criptor.io",
+      url: "https://www.criptor.io",
       changeFrequency: "always",
     },
     {
-      url: "https://criptor.io/about",
+      url: "https://wwww.criptor.io/about",
       changeFrequency: "weekly",
     },
     {
-      url: "https://criptor.io/info/cookie-policy",
+      url: "https://www.criptor.io/info/cookie-policy",
       changeFrequency: "weekly",
     },
     {
-      url: "https://criptor.io/info/disclaimer",
+      url: "https://www.criptor.io/info/disclaimer",
       changeFrequency: "weekly",
     },
     {
-      url: "https://criptor.io/info/privacy-policy",
+      url: "https://www.criptor.io/info/privacy-policy",
       changeFrequency: "weekly",
     },
     {
-      url: "https://criptor.io/info/terms-of-service",
+      url: "https://ww.criptor.io/info/terms-of-service",
       changeFrequency: "weekly",
     },
     ...publishers,
