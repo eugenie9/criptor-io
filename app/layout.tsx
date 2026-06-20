bfcf087f691d5c43c0e6c26d26997323493a9ca4
Yavuz <yavuztuncil@gmail.com>
2026-06-20 22:04:52 +0300

    Google Analytics ID hardcoded in layout.tsx instead of env var

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,17 +55,22 @@ export default function RootLayout({
       className={`${inter.variable} ${montserrat.variable} ${robotoMono.variable}`}
     >
       <head>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=G-5VQ7Z6Y23B`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-5VQ7Z6Y23B');
-          `}
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+              `}
+            </Script>
+          </>
+        )}
         </Script>
       </head>
       <body
